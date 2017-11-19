1f93c7760230a6a9efb05c4b1573b36f911d0abd
Decent Dev <dev@decent.bet>
2017-11-19 17:13:20 +0400

    Cleanup sportsbook code.

--- src/Components/v2/Dapp/Sportsbook/Sportsbook.js
+++ src/Components/v2/Dapp/Sportsbook/Sportsbook.js
@@ -7,6 +7,27 @@ import {Card, Tab, Tabs} from 'material-ui'
 
 import './sportsbook.css'
 
+const styles = {
+    inkbar: {
+        background: '#f2b45c'
+    },
+    tabItemContainer: {
+        height: 60,
+        background: 'rgba(0,0,0,0)',
+        borderBottom: 'solid 1px'
+    },
+    button: {},
+    card: (_offering) => {
+        return {
+            borderRadius: 8,
+            height: 115,
+            background: 'url(' + process.env.PUBLIC_URL + '/assets/img/offerings/' + _offering.imgUrl + ')',
+            backgroundSize: 'cover',
+            backgroundRepeat: 'none'
+        }
+    }
+}
+
 class Sportsbook extends Component {
 
     constructor(props) {
@@ -49,7 +70,7 @@ class Sportsbook extends Component {
                     <button className="navbar-toggler navbar-toggler-right" type="button"
                             data-toggle="collapse" data-target="#navbar-toggler"
                             aria-controls="navbar-toggler" aria-expanded="false">
-                        <span className="navbar-toggler-icon"></span>
+                        <span className="navbar-toggler-icon"/>
                     </button>
 
                     <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
@@ -71,49 +92,60 @@ class Sportsbook extends Component {
                     </div>
                 </nav>
             },
+            cover: () => {
+                return <div className="col-12 cover">
+                    <img src={process.env.PUBLIC_URL + '/assets/img/backgrounds/sportsbook-main.png'}/>
+                </div>
+            },
             tabs: () => {
-                return <Tabs
-                    className="tabs"
-                    inkBarStyle={{background: '#f2b45c'}}
-                    tabItemContainerStyle={{
-                        height: 60,
-                        background: 'rgba(0,0,0,0)',
-                        borderBottom: 'solid 1px'
-                    }}>
-                    <Tab
-                        label={<p className="mb-0">
-                            <span className="fa fa-heart icon mr-2"/> Favorites
-                        </p>}
-                        buttonStyle={{
-                            color: '#FEFEFE'
-                        }}/>
-                    <Tab
-                        label={<p className="mb-0">
-                            <span className="fa fa-id-badge icon mr-2"/> New
-                        </p>}
-                        buttonStyle={{
-                            color: '#FEFEFE'
-                        }}/>
-                    <Tab
-                        label={<p className="mb-0">
-                            <span className="fa fa-star icon mr-2"/> Killer Games
-                        </p>}
-                        buttonStyle={{
-                            color: '#FEFEFE'
-                        }}/>
-                </Tabs>
+                return <div className="col-6 py-2 pl-3 pr-0">
+                    <Tabs
+                        className="tabs"
+                        inkBarStyle={styles.inkbar}
+                        tabItemContainerStyle={styles.tabItemContainer}>
+                        <Tab
+                            label={<p className="mb-0">
+                                <span className="fa fa-heart icon mr-2"/> Favorites
+                            </p>}
+                            buttonStyle={styles.button}/>
+                        <Tab
+                            label={<p className="mb-0">
+                                <span className="fa fa-id-badge icon mr-2"/> New
+                            </p>}
+                            buttonStyle={styles.button}/>
+                        <Tab
+                            label={<p className="mb-0">
+                                <span className="fa fa-star icon mr-2"/> Killer Games
+                            </p>}
+                            buttonStyle={styles.button}/>
+                    </Tabs>
+                </div>
+            },
+            topRight: () => {
+                return <div className="col-6 py-2 pl-0 pr-3">
+                    <div className="top-right">
+                        <p className="text-right text-uppercase clickable">
+                            Favorite Slots <span className="fa fa-caret-right icon ml-1 mr-2"/>
+                        </p>
+                    </div>
+                </div>
+            },
+            offerings: () => {
+                return <div className="col-12">
+                    <div className="container mt-3">
+                        <div className="row">
+                            { self.state.offerings.map((_offering, index) =>
+                                self.views().offering(_offering, index)
+                            )}
+                        </div>
+                    </div>
+                </div>
             },
             offering: (_offering, index) => {
                 return <div
                     className={"col offering " + (index != (self.state.offerings.length - 1) ? "pr-1" : "pr-2")}>
                     <Card
-                        style={{
-                            borderRadius: 8,
-                            height: 115,
-                            background: 'url(' + process.env.PUBLIC_URL + '/assets/img/offerings/' + _offering.imgUrl + ')',
-                            backgroundSize: 'cover',
-                            backgroundRepeat: 'none'
-                        }}
+                        style={styles.card(_offering)}
                         className="clickable">
                         <div className="row">
                         </div>
@@ -131,35 +163,10 @@ class Sportsbook extends Component {
                 {self.views().navbar()}
                 <div className="container home">
                     <div className="row">
-                        <div className="col-12 cover">
-                            <img src={process.env.PUBLIC_URL + '/assets/img/backgrounds/sportsbook-main.png'}/>
-                        </div>
-                        <div className="col-6 py-2 pl-3 pr-0">
-                            {self.views().tabs()}
-                        </div>
-                        <div className="col-6 py-2 pl-0 pr-3">
-
-                            <div style={{
-                                height: 60,
-                                borderBottom: 'solid 1px'
-                            }}>
-                                <p className="text-right text-uppercase clickable" style={{
-                                    fontSize: 14,
-                                    paddingTop: 18
-                                }}>
-                                    Favorite Slots <span className="fa fa-caret-right icon ml-1 mr-2"/>
-                                </p>
-                            </div>
-                        </div>
-                        <div className="col-12">
-                            <div className="container mt-3">
-                                <div className="row">
-                                    { self.state.offerings.map((_offering, index) =>
-                                        self.views().offering(_offering, index)
-                                    )}
-                                </div>
-                            </div>
-                        </div>
+                        {self.views().cover()}
+                        {self.views().tabs()}
+                        {self.views().topRight()}
+                        {self.views().offerings()}
                     </div>
                 </div>
             </div>
