15cfb18847fa648fc66e7f8b936b310cb1c40ed2
Decent Dev <dev@decent.bet>
2018-01-10 01:23:28 +0400

    Remove navbar and discover from portal page

--- src/Components/Dapp/Portal/Portal.js
+++ src/Components/Dapp/Portal/Portal.js
@@ -1,8 +1,6 @@
 import React, {Component} from 'react'
 
-import Discover from './Pages/Discover/Discover'
 import SportsBook from './Pages/Sportsbook/Sportsbook'
-import Navbar from './Components/Navbar'
 
 import './portal.css'
 
@@ -12,38 +10,12 @@ class Portal extends Component {
 
     constructor(props) {
         super(props)
-        this.state = {
-            active: constants.PORTAL_PAGE_SPORTSBOOK
-        }
-    }
-
-    views = () => {
-        const self = this
-        return {
-            navbar: () => {
-                return <Navbar
-                    active={self.state.active}
-                    onSelectPage={(page) => {
-                        self.setState({
-                            active: page
-                        })
-                    }}
-                />
-            }
-        }
     }
 
     render() {
-        const self = this
         return (
             <div className="portal">
-                {self.views().navbar()}
-                {   self.state.active == constants.PORTAL_PAGE_DISCOVER &&
-                <Discover/>
-                }
-                {   self.state.active == constants.PORTAL_PAGE_SPORTSBOOK &&
                 <SportsBook/>
-                }
             </div>
         )
     }
