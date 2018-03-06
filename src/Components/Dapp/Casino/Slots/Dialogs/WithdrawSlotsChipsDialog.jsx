6a2e8cbf248c40a890d2724c9dc6621ac5e87674
Roberto E. Zubieta <roberto@decent.bet>
2018-03-06 14:11:46 -0500

    Refactored Withdraw Chips Dialog

---
+++ src/Components/Dapp/Casino/Slots/Dialogs/WithdrawSlotsChipsDialog.jsx
@@ -0,0 +1,72 @@
+import React, { Component } from 'react'
+import { Dialog, CircularProgress, FlatButton, TextField } from 'material-ui'
+
+const constants = require('../../../../Constants')
+const ethUnits = require('ethereum-units')
+
+export default class WithdrawSlotsChipsDialog extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            amount: ''
+        }
+    }
+
+    isValid = () => this.state.amount.length > 0
+
+    onCloseListener = () => this.props.toggleDialog(false)
+
+    onClickListener = () => {
+        if (this.isValid()) {
+            let ether = ethUnits
+                .convert(this.state.amount, 'ether', 'wei')
+                .toString()
+            this.props.onWithdrawChips(ether)
+        }
+    }
+
+    onValueChanged = (event, value) => this.setState({ amount: value })
+
+    renderBalanceLabel = () => {
+        if (this.props.balance != null) {
+            return this.props.balance
+        } else {
+            return <CircularProgress color={constants.COLOR_GOLD} size={18} />
+        }
+    }
+
+    render() {
+        return (
+            <Dialog
+                title="Withdraw Slots Chips"
+                actions={
+                    <FlatButton
+                        label="Get Chips"
+                        primary={true}
+                        disabled={!this.isValid()}
+                        onClick={this.onClickListener}
+                    />
+                }
+                modal={false}
+                open={this.props.open}
+                onRequestClose={this.onCloseListener}
+            >
+                <TextField
+                    floatingLabelText="Amount of Chips"
+                    fullWidth={true}
+                    type="number"
+                    value={this.state.amount}
+                    onChange={this.onValueChanged}
+                />
+                <small className="text-white">
+                    Enter the amount of chips you would like to withdraw from
+                    the Slots Manager Contract.
+                </small>
+                <br />
+                <small className="color-gold">
+                    Available slots chips: {this.renderBalanceLabel()} DBETs
+                </small>
+            </Dialog>
+        )
+    }
+}
