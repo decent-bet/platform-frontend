571f1c87f07be12b9fa5c0a6075145936b237f51
Roberto E. Zubieta <roberto@decent.bet>
2018-04-10 16:08:18 -0500

    Prettier on missing file

--- src/Components/Dapp/Portal/Sportsbook/reduxInitializationSequence.js
+++ src/Components/Dapp/Portal/Sportsbook/reduxInitializationSequence.js
@@ -4,28 +4,27 @@ import OracleBasicActions from '../../../../Model/actions/oracleBasicActions'
 import OracleGameActions from '../../../../Model/actions/oracleGameActions'
 import BettingProviderGameActions from '../../../../Model/actions/bettingProviderGameActions'
 
-export default function initializationSequence(dispatch){
-  return Promise.all([
+export default function initializationSequence(dispatch) {
+    return Promise.all([
+        dispatch(BettingProviderActions.getAddress()),
+        dispatch(BettingProviderActions.getCurrentSession()),
+        dispatch(BettingProviderActions.getHouseAddress()),
+        dispatch(BettingProviderActions.getSportsoracleAddress()),
+        dispatch(BettingProviderActions.getAllowance()),
+        dispatch(BettingProviderActions.getTokenBalance()),
+        dispatch(BettingProviderActions.getTime()),
+        dispatch(BettingProviderActions.getDepositedTokens()),
+        dispatch(BettingProviderActions.getSessionStats(1)),
 
-    dispatch(BettingProviderActions.getAddress()),
-    dispatch(BettingProviderActions.getCurrentSession()),
-    dispatch(BettingProviderActions.getHouseAddress()),
-    dispatch(BettingProviderActions.getSportsoracleAddress()),
-    dispatch(BettingProviderActions.getAllowance()),
-    dispatch(BettingProviderActions.getTokenBalance()),
-    dispatch(BettingProviderActions.getTime()),
-    dispatch(BettingProviderActions.getDepositedTokens()),
-    dispatch(BettingProviderActions.getSessionStats(1)),
+        dispatch(BetActions.getUserBets()),
 
-    dispatch(BetActions.getUserBets()),
+        dispatch(OracleBasicActions.getTime()),
+        dispatch(OracleBasicActions.getGameUpdateCost()),
+        dispatch(OracleBasicActions.getRequestedProviderAddresses()),
+        dispatch(OracleBasicActions.getAcceptedProviderAddresses()),
 
-    dispatch(OracleBasicActions.getTime()),
-    dispatch(OracleBasicActions.getGameUpdateCost()),
-    dispatch(OracleBasicActions.getRequestedProviderAddresses()),
-    dispatch(OracleBasicActions.getAcceptedProviderAddresses()),
+        dispatch(BettingProviderGameActions.getGames()),
 
-    dispatch(BettingProviderGameActions.getGames()),
-
-    dispatch(OracleGameActions.getGames())
-  ])
+        dispatch(OracleGameActions.getGames())
+    ])
 }
