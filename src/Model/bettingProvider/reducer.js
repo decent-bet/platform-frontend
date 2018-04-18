16fea2ea3385842cc77a7421445cca6dbf8bae25
Roberto E. Zubieta <roberto@decent.bet>
2018-04-18 12:22:30 -0500

    Improved betProviderReducer

--- src/Model/bettingProvider/reducer.js
+++ src/Model/bettingProvider/reducer.js
@@ -1,6 +1,8 @@
 import Actions, { Prefix } from './actionTypes'
 import { FULFILLED } from 'redux-promise-middleware'
 
+const DefaultAction = { type: null, payload: null }
+
 const DefaultBettingProviderState = {
     address: '',
     house: '0x0',
@@ -15,9 +17,91 @@ const DefaultBettingProviderState = {
     time: null
 }
 
+/**
+ * Handles the reduction of a single GameItem
+ * @param {any} gameItem
+ * @param {DefaultAction} action
+ */
+function gameItemSubReducer(gameItem = {}, action = DefaultAction) {
+    const { period } = action.meta
+    switch (action.type) {
+        case `${Prefix}/${Actions.GET_GAME_ODDS}/${FULFILLED}`:
+            return { ...gameItem, odds: action.payload }
+
+        case `${Prefix}/${Actions.GET_GAME_PERIOD_OUTCOME}/${FULFILLED}`:
+            return {
+                ...gameItem,
+                outcomes: { ...gameItem.outcomes, [period]: action.payload }
+            }
+
+        case `${Prefix}/${Actions.GET_GAME_BET_LIMIT}/${FULFILLED}`:
+            return { ...gameItem, maxBetLimit: action.payload }
+
+        case `${Prefix}/${Actions.GET_GAME_BET_LIMIT_FOR_PERIOD}/${FULFILLED}`:
+            return {
+                ...gameItem,
+                betLimits: { ...gameItem.betLimits, [period]: action.payload }
+            }
+
+        default:
+            return { ...gameItem }
+    }
+}
+
+/**
+ * Handles the reduction of a list of GameItems
+ * @param {any} gamesListState
+ * @param {DefaultAction} action
+ */
+function gamesListSubreducer(gamesListState = {}, action = DefaultAction) {
+    if (!action.meta) return { ...gamesListState }
+    const { gameId } = action.meta
+    if (gameId === undefined) return { ...gamesListState }
+
+    switch (action.type) {
+        case `${Prefix}/${Actions.GET_GAME_ITEM}/${FULFILLED}`:
+            return { ...gamesListState, [gameId]: action.payload }
+
+        default:
+            return {
+                ...gamesListState,
+                [gameId]: gameItemSubReducer(gamesListState[gameId], action)
+            }
+    }
+}
+
+/**
+ * Reduces the array of Placed Bets
+ * @param {any} placedBets
+ * @param {DefaultAction} action
+ */
+function placedBetsSubreducer(placedBets = {}, action = DefaultAction) {
+    if (!action.meta) return { ...placedBets }
+    const { gameId, betId } = action.meta
+
+    switch (action.type) {
+        case `${Prefix}/${Actions.CLAIM_BET}/${FULFILLED}`:
+            let gameBetsList = placedBets[gameId]
+            if (!gameBetsList) gameBetsList = {}
+            let newBetObject = { ...gameBetsList[betId], claimed: true }
+
+            return {
+                ...placedBets,
+                [gameId]: { ...placedBets[gameId], [betId]: newBetObject }
+            }
+        default:
+            return { ...placedBets }
+    }
+}
+
+/**
+ * Main Reducer for the BettingProvider
+ * @param {any} bettingProvider
+ * @param {DefaultAction} action
+ */
 export default function bettingProviderReducer(
     bettingProvider = DefaultBettingProviderState,
-    action = { type: null }
+    action = DefaultAction
 ) {
     switch (action.type) {
         case `${Prefix}/${Actions.GET_GAMES_COUNT}/${FULFILLED}`:
@@ -44,74 +128,6 @@ export default function bettingProviderReducer(
         case `${Prefix}/${Actions.GET_GAMES}/${FULFILLED}`:
             return { ...bettingProvider, games: action.payload }
 
-        case `${Prefix}/${Actions.GET_GAME_ITEM}/${FULFILLED}`:
-            return {
-                ...bettingProvider,
-                games: {
-                    ...bettingProvider.games,
-                    [action.meta.gameId]: action.payload
-                }
-            }
-
-        case `${Prefix}/${Actions.GET_GAME_ODDS}/${FULFILLED}`:
-            let currentId1 = action.meta.gameId
-            return {
-                ...bettingProvider,
-                games: {
-                    ...bettingProvider.games,
-                    [currentId1]: {
-                        ...bettingProvider.games[currentId1],
-                        odds: action.payload
-                    }
-                }
-            }
-
-        case `${Prefix}/${Actions.GET_GAME_PERIOD_OUTCOME}/${FULFILLED}`:
-            let currentId2 = action.meta.gameId
-            return {
-                ...bettingProvider,
-                games: {
-                    ...bettingProvider.games,
-                    [currentId2]: {
-                        ...bettingProvider[currentId2],
-                        outcomes: {
-                            ...bettingProvider[currentId2].outcomes,
-                            [action.meta.period]: action.payload
-                        }
-                    }
-                }
-            }
-
-        case `${Prefix}/${Actions.GET_GAME_BET_LIMIT}/${FULFILLED}`:
-            let currentId3 = action.meta.gameId
-            return {
-                ...bettingProvider,
-                games: {
-                    ...bettingProvider.games,
-                    [currentId3]: {
-                        ...bettingProvider.games[currentId3],
-                        maxBetLimit: action.payload
-                    }
-                }
-            }
-
-        case `${Prefix}/${Actions.GET_GAME_BET_LIMIT_FOR_PERIOD}/${FULFILLED}`:
-            let currentId4 = action.meta.gameId
-            let periodId1 = action.meta.period
-            return {
-                ...bettingProvider,
-                games: {
-                    ...bettingProvider.games,
-                    [currentId4]: {
-                        ...bettingProvider.games[currentId4],
-                        betLimits: {
-                            ...bettingProvider.games[currentId4].betLimits,
-                            [periodId1]: action.payload
-                        }
-                    }
-                }
-            }
-
         case `${Prefix}/${Actions.GET_USER_BETS}/${FULFILLED}`:
             return { ...bettingProvider, placedBets: action.payload }
 
@@ -119,28 +135,17 @@ export default function bettingProviderReducer(
         case `${Prefix}/${Actions.SET_TIME}`:
             return { ...bettingProvider, time: action.payload }
 
-        case `${Prefix}/${Actions.CLAIM_BET}/${FULFILLED}`:
-            let currentId5 = action.meta.gameId
-            let currentBet1 = action.meta.betId
-            let newBetObject = {
-                ...bettingProvider.placedBets[currentId5][currentBet1],
-                claimed: true
-            }
-            return {
-                ...bettingProvider,
-                placedBets: {
-                    ...bettingProvider.placedBets,
-                    [currentId5]: {
-                        ...bettingProvider.placedBets[currentId5],
-                        [currentBet1]: newBetObject
-                    }
-                }
-            }
-
         case `${Prefix}/${Actions.GET_ADDRESS}/${FULFILLED}`:
             return { ...bettingProvider, address: action.payload }
 
         default:
-            return { ...bettingProvider }
+            return {
+                ...bettingProvider,
+                games: gamesListSubreducer(bettingProvider.games, action),
+                placedBets: placedBetsSubreducer(
+                    bettingProvider.placedBets,
+                    action
+                )
+            }
     }
 }
