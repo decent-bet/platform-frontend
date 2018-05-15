5bb93c3f5839b2875d97a506561d695145a7d4da
Roberto E. Zubieta <roberto@decent.bet>
2018-05-15 12:10:56 -0500

    StateChannelBuilder now uses a TextField

--- src/Components/Dapp/Casino/Slots/StateChannelBuilder.jsx
+++ src/Components/Dapp/Casino/Slots/StateChannelBuilder.jsx
@@ -1,40 +1,62 @@
 import React, { Component } from 'react'
-import { Card, RaisedButton, CardActions, CardHeader } from 'material-ui'
+import {
+    Card,
+    RaisedButton,
+    CardActions,
+    CardHeader,
+    CardText,
+    TextField
+} from 'material-ui'
 import ethUnits from 'ethereum-units'
 
 export default class StateChannelBuilder extends Component {
-    on100DbetChannelClicked = () => {
-        const { onBuildChannelListener } = this.props
-        onBuildChannelListener(ethUnits.convert(100, 'ether', 'wei'))
+    state = {
+        value: ''
     }
 
-    on150DbetChannelClicked = () => {
+    onValueChanged = (event, value) => this.setState({ value })
+
+    commit = () => {
+        // Do not Commit if value is invalid
+        if (!this.isValueValid()) return
+
         const { onBuildChannelListener } = this.props
-        onBuildChannelListener(ethUnits.convert(150, 'ether', 'wei'))
+        const finalValue = ethUnits.convert(this.state.value, 'ether', 'wei')
+        onBuildChannelListener(finalValue)
+    }
+
+    isValueValid = () => {
+        const parsedValue = parseInt(this.state.value, 10)
+        return parsedValue >= 100 && parsedValue <= 1000
     }
 
     render() {
+        const errorText = this.isValueValid()
+            ? null
+            : 'Between [100 and 1000]'
         return (
-            <section>
-                <Card className="card">
-                    <CardHeader
-                        title="Create a new State Channel"
-                        subtitle="How much you want to DBET?"
+            <Card className="card">
+                <CardHeader
+                    title="Let's play Slots"
+                    subtitle="How many DBETs would you like?"
+                />
+                <CardText>
+                    <TextField
+                        name="initial-deposit"
+                        value={this.state.value}
+                        onChange={this.onValueChanged}
+                        fullWidth={true}
+                        errorText={errorText}
                     />
-                    <CardActions>
-                        <RaisedButton
-                            primary={true}
-                            label="100 DBET session"
-                            onClick={this.on100DbetChannelClicked}
-                        />
-                        <RaisedButton
-                            primary={true}
-                            label="150 DBET session"
-                            onClick={this.on150DbetChannelClicked}
-                        />
-                    </CardActions>
-                </Card>
-            </section>
+                </CardText>
+                <CardActions>
+                    <RaisedButton
+                        primary={true}
+                        label="Go!"
+                        onClick={this.commit}
+                    />
+                </CardActions>
+            </Card>
         )
     }
 }
