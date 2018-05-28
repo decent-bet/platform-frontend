23c4c47f65bd5c594ac4941c0455bee4d4fb698e
Roberto E. Zubieta <roberto@decent.bet>
2018-05-28 14:42:56 -0500

    Removed deprecated styles

--- src/Components/Base/Themes.js
+++ src/Components/Base/Themes.js
@@ -4,11 +4,10 @@
 
 import getMuiTheme from 'material-ui/styles/getMuiTheme'
 import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme'
-
-const constants = require('./../Constants')
+import * as constants from './../Constants'
 
 // Main Theme for all the Application
-const MainTheme = getMuiTheme(darkBaseTheme, {
+export const MainTheme = getMuiTheme(darkBaseTheme, {
     palette: {
         primary1Color: constants.COLOR_GOLD,
         primary2Color: constants.COLOR_GOLD_DARK,
@@ -22,40 +21,5 @@ const MainTheme = getMuiTheme(darkBaseTheme, {
     },
     button: {
         textTransform: 'capitalize'
-    },
-})
-
-// DEPRECATED: Theme used for the snackbar
-const SnackbarTheme = getMuiTheme({
-    palette: {
-        textColor: constants.COLOR_PRIMARY,
-        alternateTextColor: constants.COLOR_WHITE,
-        primary1Color: constants.COLOR_PRIMARY,
-        primary2Color: constants.COLOR_PRIMARY_DARK,
-        accent1Color: constants.COLOR_RED,
-        canvasColor: constants.COLOR_PRIMARY
     }
 })
-
-// DEPRECATED: Used for the red Buttons
-const ButtonsTheme = getMuiTheme({
-    palette: {
-        textColor: constants.COLOR_GOLD,
-        alternateTextColor: constants.COLOR_GOLD,
-        primary1Color: constants.COLOR_GOLD
-    }
-})
-
-// DEPRECATED: Used for certain dialogs
-const DialogTheme = getMuiTheme({
-    palette: {
-        textColor: '#ffffff',
-        alternateTextColor: '#F0AD4E',
-        primary1Color: constants.COLOR_GOLD,
-        primary2Color: constants.COLOR_GOLD,
-        accent1Color: constants.COLOR_RED,
-        canvasColor: constants.COLOR_PRIMARY_DARK
-    }
-})
-
-export { MainTheme, SnackbarTheme, DialogTheme, ButtonsTheme }
