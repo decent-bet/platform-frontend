6066dd5ebcfd1d3ad29d718a8e2adad6ccdacced
Roberto E. Zubieta <roberto@decent.bet>
2018-06-11 16:25:57 -0500

    Fixed broken function

--- src/Components/Dapp/Dashboard/FaucetMenuItem.jsx
+++ src/Components/Dapp/Dashboard/FaucetMenuItem.jsx
@@ -13,24 +13,25 @@ const helper = new Helper()
 const provider = helper.getGethProvider()
 
 export default function FaucetMenuItem({ onFaucetClickedListener }) {
-    if (provider !== PROVIDER_DBET) {
-        return (
-            <ListItem
-                button
-                className="menu-item"
-                onClick={onFaucetClickedListener}
-            >
-                <ListItemIcon>
-                    <SvgIcon>
-                        <FontAwesomeIcon
-                            icon={['fab', 'ethereum']}
-                            className="fa-fw"
-                        />
-                    </SvgIcon>
-                </ListItemIcon>
-
-                <ListItemText>Claim Faucet</ListItemText>
-            </ListItem>
-        )
+    if (provider === PROVIDER_DBET) {
+        return null
     }
+    return (
+        <ListItem
+            button
+            className="menu-item"
+            onClick={onFaucetClickedListener}
+        >
+            <ListItemIcon>
+                <SvgIcon>
+                    <FontAwesomeIcon
+                        icon={['fab', 'ethereum']}
+                        className="fa-fw"
+                    />
+                </SvgIcon>
+            </ListItemIcon>
+
+            <ListItemText>Claim Faucet</ListItemText>
+        </ListItem>
+    )
 }
