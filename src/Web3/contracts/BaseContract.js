915f5c25683bef66d90193311f7f0db4dc8bdec0
dev <dev@decent.bet>
2018-08-20 04:24:41 +0530

    Fix: Manually sort new channel events on calling getPastEvents() since order: "DESC" doesn't work

--- src/Web3/contracts/BaseContract.js
+++ src/Web3/contracts/BaseContract.js
@@ -32,7 +32,7 @@ export default class BaseContract {
             {
                 range: {},
                 options,
-                order: "ASC"
+                order: "DESC"
             }
         )
     }
