4d7b959878534e21707f86bde8d328f5d7a17df5
Bradley Suira <bradleysuira@gmail.com>
2018-10-28 22:17:52 -0500

    changed dockerfile to use stages

--- docker-build.local.sh
+++ docker-build.local.sh
@@ -2,7 +2,7 @@
 VERSION=$1
 
 echo "Building version $VERSION"
-docker build -t dbet-platform/platform-frontend:latest .
+docker build --build-arg APP_ENV=$APP_ENV -t dbet-platform/platform-frontend:latest .
 docker tag dbet-platform/platform-frontend:latest dbet-platform/platform-frontend:latest
 echo "done $VERSION"
 docker-compose up --force-recreate
