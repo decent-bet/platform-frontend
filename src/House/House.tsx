fc4366d5c0d3b1bfa3472a8950b612a69c3e3714
Roberto E. Zubieta <roberto@decent.bet>
2018-11-30 16:23:13 -0500

    Lorem Ipsum Page

--- src/House/House.tsx
+++ src/House/House.tsx
@@ -1,8 +1,35 @@
 import React, { Component } from 'react'
+import {
+    Card,
+    CardHeader,
+    CardContent,
+    Grid,
+    Paper,
+    Typography
+} from '@material-ui/core'
 
 class House extends Component<{}, {}> {
     public render() {
-        return <h1>Lorem Ipsum</h1>
+        return (
+            <Grid container={true} justify="center" direction="row">
+                <Grid item={true} xs={12} md={8} lg={6}>
+                    <Paper>
+                        <Card>
+                            <CardHeader
+                                title="House Status"
+                                subheader="Dolor sic amet"
+                            />
+                            <CardContent>
+                                <Typography>
+                                    Lorem Ipsum
+                                    acjbasjkbajcbasjbasbcsajcbasjcbsakjcbsakcbasjcbsackjbasckjbaskcbasc
+                                </Typography>
+                            </CardContent>
+                        </Card>
+                    </Paper>
+                </Grid>
+            </Grid>
+        )
     }
 }
 
